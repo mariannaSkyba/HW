5bc2329f95dff6d52c2ab791966416219b6b416d
Marianna Skyba <marianass123@gmail.com>
2021-04-10 09:13:52 +0300

    Commit

---
+++ homework9/script.js
@@ -0,0 +1,11 @@
+// let message = "hello world";
+
+// const number = 2;
+
+// var test = `Мені ${number} age `;
+// console.log(test);
+// console.warn(test);
+// console.error(test);
+
+let message = confirm("Are you ok ?");
+console.warn(message);
